9c35a66fb631027941bdc7532fa69ad480ca3f98
Taiji-Kondo <k.taiji1108@gmail.com>
2025-10-15 08:20:37 +0900

    Agents random walk using array

--- src/sketches/array.ts
+++ src/sketches/array.ts
@@ -3,7 +3,8 @@ import p5 from 'p5';
 const base = (p: p5) => {
   const width = 800
   const height = 800
-  const pos = {x: width / 2, y: height / 2} satisfies {x: number, y: number}
+  const initial = {x: width / 2, y: height / 2, size: 0, color: 255} satisfies {x: number, y: number, size: number, color: number}
+  const agents = Array.from({length: 1000}, () => ({...initial, size: p.random(20, 50), color: p.random(0, 100)})) satisfies typeof initial[]
 
   p.setup = () => {
     p.createCanvas(width, height);
@@ -12,11 +13,14 @@ const base = (p: p5) => {
 
   p.draw = () => {
     p.background(220)
-    pos.x = pos.x + p.random(-10, 10)
-    pos.y = pos.y + p.random(-10, 10)
 
-    p.fill(0)
-    p.ellipse(pos.x, pos.y, 20, 20)
+    for (let i = 0; i < agents.length; i++) {
+      agents[i].x = agents[i].x + p.random(-10, 10)
+      agents[i].y = agents[i].y + p.random(-10, 10)
+
+      p.fill(agents[i].color, 0, 0)
+      p.ellipse(agents[i].x, agents[i].y, agents[i].size, agents[i].size)
+    }
   };
 };
 
