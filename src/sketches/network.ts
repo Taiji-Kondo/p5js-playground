81f20dcb9e2a8b4695c2055500d8eee9248ebb4b
Taiji-Kondo <k.taiji1108@gmail.com>
2025-10-16 08:48:28 +0900

    Refactor network sketch to modularize node rendering and add multi-layer visualization.

--- src/sketches/network.ts
+++ src/sketches/network.ts
@@ -3,11 +3,30 @@ import p5 from 'p5';
 const network = (p: p5) => {
   const dots: {x: number, y: number}[] = []
 
+  const createNetwork = (dotsArray: typeof dots, level: number) => {
+    dotsArray.forEach((dot) => {
+      const alpha = p.map(10 - level * level, 10,0, 255, 0)
+
+      p.fill(255, 255, 255, alpha)
+      p.ellipse(dot.x, dot.y, 10, 10);
+
+      dotsArray.forEach((otherDot) => {
+        if (dot === otherDot) return;
+
+        const distance = p.dist(dot.x, dot.y, otherDot.x, otherDot.y);
+        if (distance > 200) return;
+
+        p.stroke(255, 255, 255, alpha)
+        p.line(dot.x, dot.y, otherDot.x, otherDot.y);
+      });
+    });
+  }
+
   p.setup = () => {
     p.createCanvas(800, 800);
     p.background(0);
 
-    for (let i = 0; i < 50; i++) {
+    for (let i = 0; i < 200; i++) {
       dots.push({x: p.random(p.width), y: p.random(p.height)})
     }
   };
@@ -15,20 +34,9 @@ const network = (p: p5) => {
   p.draw = () => {
     p.background(0);
 
-    dots.forEach((dot) => {
-      p.fill(255)
-      p.ellipse(dot.x, dot.y, 10, 10);
-
-      dots.forEach((otherDot) => {
-        if (dot === otherDot) return;
-
-        const distance = p.dist(dot.x, dot.y, otherDot.x, otherDot.y);
-        if (distance > 200) return;
-
-        p.stroke(255)
-        p.line(dot.x, dot.y, otherDot.x, otherDot.y);
-      });
-    });
+    createNetwork(dots.slice(0, 50), 1)
+    createNetwork(dots.slice(50, 100), 2)
+    createNetwork(dots.slice(100, 200), 3)
   };
 };
 
