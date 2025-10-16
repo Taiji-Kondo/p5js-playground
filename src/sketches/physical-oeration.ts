6d9a19c5ba4ea2387060d799669ebfc36d535b12
Taiji-Kondo <k.taiji1108@gmail.com>
2025-10-16 20:20:07 +0900

    Add animated ellipse with controllable speed to physicalOperation sketch

--- src/sketches/physical-oeration.ts
+++ src/sketches/physical-oeration.ts
@@ -1,12 +1,40 @@
 import p5 from 'p5';
 
 const physicalOperation = (p: p5) => {
+  const ellipseSize = 50;
+  const ellipseSizeHalf = ellipseSize / 2;
+
+  let positionX = 0
+  let speed = 1
+
   p.setup = () => {
-    p.createCanvas(400, 400);
-    p.background(0)
+    p.createCanvas(800, 800);
   };
 
-  p.draw = () => {};
+  p.draw = () => {
+    p.background(240, 240, 240)
+
+    p.fill(255, 255, 0)
+    p.ellipse(positionX, p.height / 2, ellipseSize, ellipseSize)
+
+    if (positionX > p.width + ellipseSizeHalf) {
+      positionX = 0 - ellipseSizeHalf
+    }
+    if (positionX < 0 - ellipseSizeHalf) {
+      positionX = p.width + ellipseSizeHalf
+    }
+
+    positionX += speed
+  };
+
+  p.keyPressed = () => {
+    if (p.key === 'ArrowRight') {
+      speed += 1
+    }
+    if (p.key === 'ArrowLeft') {
+      speed -= 1
+    }
+  }
 };
 
 export default physicalOperation;
